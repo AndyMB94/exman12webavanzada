f6be3a890d70078af4ac66b7230dfbba4f2f3e91
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2025-06-07 19:00:46 -0500

    fix: corregido tipo en API [id]

--- src/app/api/medicamentos/[id]/route.ts
+++ src/app/api/medicamentos/[id]/route.ts
@@ -3,35 +3,32 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
+interface Context {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(_req: Request, context: Context) {
   const medicamento = await prisma.medicamento.findUnique({
-    where: { CodMedicamento: Number(params.id) },
+    where: { CodMedicamento: Number(context.params.id) },
     include: { tipoMedic: true },
   });
   return NextResponse.json(medicamento);
 }
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: Request, context: Context) {
   const data = await req.json();
   const actualizado = await prisma.medicamento.update({
-    where: { CodMedicamento: Number(params.id) },
+    where: { CodMedicamento: Number(context.params.id) },
     data,
   });
   return NextResponse.json(actualizado);
 }
 
-export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(_req: Request, context: Context) {
   await prisma.medicamento.delete({
-    where: { CodMedicamento: Number(params.id) },
+    where: { CodMedicamento: Number(context.params.id) },
   });
   return NextResponse.json({ message: 'Eliminado correctamente' });
 }
